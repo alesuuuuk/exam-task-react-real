f5129e7d2e030de85aae66e445d12249bcc856c1
ilovefish3801 <pipipupi.kom@gmail.com>
2023-11-29 19:58:30 +0200

    a

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
 // style
 import s from "./Header.module.scss"
 
 const Header = () => {
   return (
     <header>
-        header
+        <Link href={"/"} className={s.Logo}>LOGO</Link>
+
+        <ul>
+          <li>
+            <Link href={"/"}></Link>
+          </li>
+          <li>
+            <Link href={"/"}></Link>
+          </li>
+          <li>
+            <Link href={"/"}></Link>
+          </li>
+          <li>
+            <Link href={"/"}></Link>
+          </li>
+        </ul>
     </header>
   )
 }
