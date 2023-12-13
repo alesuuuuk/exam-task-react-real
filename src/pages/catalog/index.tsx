7681576431dd75b7b7abc3210638e29b99cabc92
school computer <aslesyk@gmail.com>
2023-12-13 17:09:14 +0200

    commit

--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -25,6 +25,7 @@ const Catalog = ({ prods }: Props) => {
   // let testA = [{id: 1}, {id: 2},{id: 3},{id: 4},{id: 5},{id: 6},{id: 7},{id: 8},{id: 9},{id: 10},{id: 11},{id: 12},{id: 13},{id: 14},{id: 15},{id: 16},{id: 17},{id: 18},{id: 19},{id: 20},{id: 21},{id: 22}]
   prods.map((e)=>{
     if (counter < 9){
+      {/*@ts-ignore*/}
       pagPagesArray.push(e)
       // console.log(e.id)
     }else{
